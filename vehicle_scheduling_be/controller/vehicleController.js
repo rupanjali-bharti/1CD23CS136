2d4a0f8cae679ed4a6abbf819884f07ca89c695f
Rupanjali Bharti <rupanjalibharti070@gmail.com>
2026-06-29 12:02:40 +0530

    initialized the vehicle scheduling folder structure

---
+++ vehicle_scheduling_be/controller/vehicleController.js
@@ -0,0 +1,1 @@
+// to manage the apis 
---
+++ vehicle_scheduling_be/scheduler/vehicleScheduler.js
@@ -0,0 +1,36 @@
+
+function maximizeImpact(tasks, budget) {
+    const n = tasks.length;
+    const dp = Array(n + 1).fill(0).map(() => Array(budget + 1).fill(0));
+
+    for (let i = 1; i <= n; i++) {
+        const task = tasks[i - 1];
+        const weight = task.Duration; 
+        const value = task.Impact;
+
+        for (let w = 1; w <= budget; w++) {
+            if (weight <= w) {
+                dp[i][w] = Math.max(dp[i - 1][w], dp[i - 1][w - weight] + value);
+            } else {
+                dp[i][w] = dp[i - 1][w];
+            }
+        }
+    }
+
+    let res = dp[n][budget];
+    let w = budget;
+    const selectedTasks = [];
+
+    for (let i = n; i > 0 && res > 0; i--) {
+        if (res !== dp[i - 1][w]) {
+            const task = tasks[i - 1];
+            selectedTasks.push(task.TaskID); 
+            res -= task.Impact;
+            w -= task.Duration;
+        }
+    }
+
+    return { totalImpact: dp[n][budget], timeUsed: budget - w, tasks: selectedTasks };
+}
+
+module.exports = { maximizeImpact };
